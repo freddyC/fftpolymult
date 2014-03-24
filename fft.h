9ed2f4d91e70ea6ae6aa5d221988f2ddb46b73d7
Fred <fred.usu@gmail.com>
2014-03-24 10:41:48 -0600

    got both algorithms running and good.  Now reports

--- fft.h
+++ fft.h
@@ -5,63 +5,117 @@
 #include <complex>
 #include <cmath>
 #include <utility>
+#include <iostream>
 
 namespace fft
 {
   const double PI = 3.14159265359;
-
+  
   std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > getOmegasForSize (int n) {
     std::vector<std::complex<double> > omega(n);
     std::vector<std::complex<double> > invOmega(n);
-
-    for (int i = 0; i < n; ++i) {
+    
+    for (auto i = 0; i < n; ++i) {
       omega[i] = std::complex<double> (std::cos((2 * PI * i)/n), std::sin((2 * PI * i)/n));
       invOmega[i] = std::complex<double> (std::cos((2 * PI * i)/n), -1 * std::sin((2 * PI * i)/n));
     }
     return std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > (omega, invOmega);
   }
-
-  std::vector<std::complex<double> > fftRecursive(std::vector<std::complex<double> > &poly, int power, bool useOmega, std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > &omegas) {
-    // BASE CASE
+  
+  
+  int rbs(int i, int k) {
+    if (k == 0) {
+      return i;
+    } else if (i %2 == 1) {
+      return std::pow(2, k - 1) + rbs(i / 2, k - 1);
+    }
+    return rbs(i / 2, k - 1);
+  }
+  
+  std::vector<std::vector<int> > getRbsCache (int size) {
+    int logN = std::log(size) / std::log(2);
+    std::vector<std::vector<int> > res(size, std::vector<int> (logN));
+    for (auto i = 0; i < size; ++i) {
+      for (auto j = 0; j <= logN; ++j) {
+        res[i][j] = rbs(i, logN);
+      }
+    }
+    return res;
+  }
+  
+  std::vector<std::complex<double> > fftRecursive(std::vector<std::complex<double> > &poly, long long int power, std::vector< std::complex<double> > &omegas) {
+      // BASE CASE
     if (poly.size() == 1) {
       return poly;
     }
-
-    // Divide into two parts
+    
+      // Divide into two parts
     int n = poly.size();
     std::vector<std::complex<double> > evens;
     std::vector<std::complex<double> > odds;
-    for (int i = 0; i < n; ++i) {
-      if (i % 2 == 0) {
+    for (auto i = 0; i < n; ++i) {
+      if (i %2 == 0) {
         evens.push_back(poly[i]);
       } else {
         odds.push_back(poly[i]);
       }
     }
-
-    // Recurse
-    auto solvedE = fftRecursive(evens, power * 2, useOmega, omegas);
-    auto solvedO = fftRecursive(odds, power * 2, useOmega, omegas);
-
-    // Construct Results
-   std::vector<std::complex<double> > result;
-    for (int i = 0; i < n/2; ++i) {
-      if (useOmega) {
-        result.push_back(solvedE[i] + (omegas.first[i * power] * solvedO[i] ) );
-      } else {
-        result.push_back(solvedE[i] + (omegas.second[i * power] * solvedO[i] ) );
-      }
+    
+      // Recurse
+    auto solvedE = fftRecursive(evens, power * 2, omegas);
+    auto solvedO = fftRecursive(odds, power * 2, omegas);
+    
+      // Construct Results
+    std::vector<std::complex<double> > result;
+    std::vector<std::complex<double> > right;
+    
+    for (auto i = 0; i < n/2; ++i) {
+      right.push_back(omegas[i * power] * solvedO[i] );
     }
-    for (int i = 0; i < n/2; ++i) {
-      if (useOmega) {
-        result.push_back(solvedE[i] - (omegas.first[i * power] * solvedO[i] ) );
-      } else {
-        result.push_back(solvedE[i] - (omegas.second[i * power] * solvedO[i] ) );
-      }
+    
+    for (auto i = 0; i < n/2; ++i) {
+      result.push_back(solvedE[i] + right[i]);
+    }
+    for (auto i = 0; i < n/2; ++i) {
+      result.push_back(solvedE[i] - right[i]);
     }
     return result;
   }
-
+  
+  std::vector<std::complex<double> > fftDynamic (std::vector<std::complex<double> > &poly, std::vector< std::complex<double> > &omegas, std::vector<std::vector<int> > &rbsCache) {
+    int n = poly.size();
+    int logN = std::log(n) / std::log(2);
+    
+      // set up 2d vector of solutions
+    std::vector<std::vector<std::complex<double> > > sol(2, std::vector<std::complex<double> > (n, std::complex<double> (0,0)));
+    
+      // Add in bottom row (base case)
+    for (auto i = 0; i < n; ++i) {
+      sol[0][rbsCache[i][logN]] = poly[i];
+    }
+    
+      // number of omega slots at the bottom
+    int power = n/2;
+    int size = 2;
+    
+      // scan from bottom to top
+    for (auto k = 1; k <= logN; ++k) {
+        // scan accross by size for each sub solution
+      for (auto i = 0; i < n; i += size) {
+        std::complex<double> odd;
+          // fill in this solution
+        for (auto j = 0; j < size/2; ++j) {
+          odd = omegas[j*power] * sol[(k-1) %2][i+j+(size/2)];
+          sol[k %2][i+j] = sol[(k-1) %2][i+j] + odd;
+          sol[k %2][i+j+(size/2)] = sol[(k-1) %2][i+j] - odd;
+        }
+      }
+        // decrease power and increase size
+      power /= 2;
+      size *=2;
+    }
+    return sol[logN %2];
+  }
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -9,11 +9,12 @@
 #include <complex>
 #include <cmath>
 #include <string>
+#include <fstream>
 #include <sstream>
 
-std::pair<double, double> solveIt (int length, bool recursive);
-std::vector<double> recursivePolyMult (std::vector<double>, std::vector<double>, int n, std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > &omegas);
-// std::vector<double> dynamicPolyMult (std::vector<double>, std::vector<double>, int n, std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > &omegas);
+std::pair<double, double> solveIt (long long int length, bool recursive);
+std::vector<double> recursivePolyMult (std::vector<double> &polyA, std::vector<double> &polyB, int n, std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > &omegas);
+std::vector<double> dynamicPolyMult (std::vector<double> &polyA, std::vector<double> &polyB, int n, std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > &omegas, std::vector<std::vector<int> > &rbsCache);
 void padPoly (std::vector<double> &poly, std::vector< std::complex <double> > &complex);
 std::string writeVector (std::vector <double> );
 std::string writeVector (std::vector <std::complex <double> >);
@@ -21,92 +22,131 @@ double randomDouble();
 
 
 int main () {
-
-  std::cout << "\nRecursive Results\n";
-  for (long long int i = 2; i < std::pow(2, 20) +1; i *= 2) {
-    auto timeTook = solveIt(i, true);
-    std::cout << "\n Terms: " << i << " Average: " << timeTook.first << " Deviation: " << timeTook.second << "\n";
+    // Do the recursive
+    // std::ofstream foutR ("/Users/fred.christensen/Dropbox/school/Algorithms/fftpolymult/recursive.csv");
+    // foutR << "Polynomial Length, Time Took" << std::endl;
+    // std::cout << "\nRecursive Results\n";
+    // for (auto i = 2; i < std::pow(2, 20) +1; i *= 2) {
+    //   auto timeTook = solveIt(i, true);
+    //   std::cout << "\n Terms: " << i << " Average: " << timeTook.first << "\n";
+    // foutR << i << ", " << timeTook.first << std::endl;
+    // }
+    // foutR.close();
+  
+    // do the dynamic
+  std::ofstream foutD ("/Users/fred.christensen/Dropbox/school/Algorithms/fftpolymult/dynamic.csv");
+  foutD << "Polynomial Length, Time Took" << std::endl;
+  std::cout << "\nDynamic Results\n";
+  for (auto i = 2; i < std::pow(2, 20) +1; i *= 2) {
+    auto timeTook = solveIt(i, false);
+    foutD << i << ", " << timeTook.first << std::endl;
+    std::cout << "\n Terms: " << i << " Average: " << timeTook.first << "\n";
   }
-
-  // std::cout << "\nDynamic Results\n";
-  // for (long long int i = 2; i < std::pow(2, 20) +1; i *= 2) {
-  //   auto timeTook = solveIt(i, false);
-  //   std::cout << "\n Terms: " << i << " Average: " << timeTook.first << " Deviation: " << timeTook.second << "\n";
-  // }
-
+  foutD.close();
   return 0;
 }
 
-std::pair<double, double> solveIt(int size, bool recursive) {
+std::pair<double, double> solveIt(long long int size, bool recursive) {
   std::vector<double> x;
   std::vector<double> y;
   auto omegas = fft::getOmegasForSize(size * 2);
-
-  // if (recursive) {
+  
+  if (recursive) {
     return tools::funcEval([&] () {
-      for (int i = 0; i < size; ++i) {
+      x.clear();
+      y.clear();
+      for (auto i = 0; i < size; ++i) {
         x.push_back(randomDouble());
         y.push_back(randomDouble());
       }
       recursivePolyMult(x, y, size, omegas);
-    }, 5);
-  // }
-  // return tools::funcEval([&] () {
-  //   for (int i = 0; i < size; ++i) {
-  //     x[i] = randomDouble();
-  //     y[i] = randomDouble();
-  //   }
-  //   dynamicPolyMult(x, y, size)
-  // });
+    });
+  }
+  
+  auto rbsCache = fft::getRbsCache(size * 2);
+  return tools::funcEval([&] () {
+    x.clear();
+    y.clear();
+    for (auto i = 0; i < size; ++i) {
+      x.push_back(randomDouble());
+      y.push_back(randomDouble());
+    }
+    dynamicPolyMult(x, y, size, omegas, rbsCache);
+  });
 }
 
 
-std::vector<double> recursivePolyMult(std::vector<double> polyA, std::vector<double> polyB, int n, std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > &omegas) {
-  /// Double the size and pad with 0's
+std::vector<double> recursivePolyMult (std::vector<double> &polyA, std::vector<double> &polyB, int n, std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > &omegas) {
+    /// Double the size and pad with 0's
   std::vector<std::complex<double> > complexA;
   std::vector<std::complex<double> > complexB;
   padPoly(polyA, complexA);
   padPoly(polyB, complexB);
-
-  // fftRecursive each polynomial
-  auto solveA = fft::fftRecursive(complexA, 1, true, omegas);
-  auto solveB = fft::fftRecursive(complexB, 1, true, omegas);
-
-  // multiply the evaluated polynomials
+  
+    // fftRecursive each polynomial
+  auto solveA = fft::fftRecursive(complexA, 1, omegas.first);
+  auto solveB = fft::fftRecursive(complexB, 1, omegas.first);
+  
+    // multiply the evaluated polynomials
   std::vector<std::complex<double> > res;
-  for (int i = 0; i < solveA.size(); ++i) {
+  for (auto i = 0; i < solveA.size(); ++i) {
     res.push_back(solveA[i] * solveB[i]);
   }
-
-  // invers fft
-  auto complexResult = fft::fftRecursive(res, 1, false, omegas);
-
-  // divide by 2n
+  
+    // invers fft
+  auto complexResult = fft::fftRecursive(res, 1, omegas.second);
+  
+    // divide by 2n
   std::vector<double> result;
-  for(int i = 0; i < complexResult.size() - 1; ++i ) {
+  for (auto i = 0; i < complexResult.size() - 1; ++i ) {
     result.push_back(complexResult[i].real() / (2 * n));
   }
-
-  // TO SEE RESULTS UNCOMENT ME!!
-  // std::cout << "\nResults: \n(" << writeVector(polyA) << ")*(" << writeVector(polyB) << ") = (" << writeVector(result) << ")\n\n";
+  
+    // TO SEE RESULTS UNCOMENT ME!!
+    // std::cout << "\nResults: \n(" << writeVector(polyA) << ")*(" << writeVector(polyB) << ") = (" << writeVector(result) << ")\n\n";
   return result;
 }
 
 
 
-// std::vector<double> dynamicPolyMult (std::vector<double>, std::vector<double>, int n, std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > &omegas) {
-
-
-//   return ;
-// }
+std::vector<double> dynamicPolyMult (std::vector<double> &polyA, std::vector<double> &polyB, int n, std::pair< std::vector< std::complex<double> >, std::vector< std::complex<double> > > &omegas, std::vector<std::vector<int> > &rbsCache) {
+    // Double the size and pad with 0's
+  std::vector<std::complex<double> > complexA;
+  std::vector<std::complex<double> > complexB;
+  padPoly(polyA, complexA);
+  padPoly(polyB, complexB);
+  
+    // fftRecursive each polynomial
+  auto solveA = fft::fftDynamic(complexA, omegas.first, rbsCache);
+  auto solveB = fft::fftDynamic(complexB, omegas.first, rbsCache);
+  
+    // multiply the evaluated polynomials
+  std::vector<std::complex<double> > res;
+  for (auto i = 0; i < solveA.size(); ++i) {
+    res.push_back(solveA[i] * solveB[i]);
+  }
+  
+    // invers fft
+  auto complexResult = fft::fftDynamic(res, omegas.second, rbsCache);
+  
+    // divide by 2n
+  std::vector<double> result;
+  for (auto i = 0; i < complexResult.size() - 1; ++i ) {
+    result.push_back(complexResult[i].real() / (2 * n));
+  }
+  
+    // TO SEE RESULTS UNCOMENT ME!!
+    // std::cout << "\nResults: \n(" << writeVector(polyA) << ")*(" << writeVector(polyB) << ") = (" << writeVector(result) << ")\n\n";
+  return result;
+}
 
 
 void padPoly(std::vector<double> &poly, std::vector< std::complex <double> > &complex) {
   complex.clear();
-  for (int i = 0; i < poly.size(); ++i) {
+  for (auto i = 0; i < poly.size(); ++i) {
     complex.push_back(std::complex<double> (poly[i], 0));
   }
-  for (int i = 0; i < poly.size(); ++i) {
+  for (auto i = 0; i < poly.size(); ++i) {
     complex.push_back(std::complex<double> (0,0));
   }
 }
@@ -114,7 +154,7 @@ void padPoly(std::vector<double> &poly, std::vector< std::complex <double> > &co
 std::string writeVector (std::vector <double> arr) {
   std::stringstream ss;
   ss << arr[0];
-  for (int i = 1; i < arr.size(); ++i) {
+  for (auto i = 1; i < arr.size(); ++i) {
     ss << " + " << arr[i] << "x^" << i;
   }
   return ss.str();
@@ -123,7 +163,7 @@ std::string writeVector (std::vector <double> arr) {
 std::string writeVector (std::vector <std::complex <double> > arr) {
   std::stringstream ss;
   ss << arr[0].real();
-  for (int i = 1; i < arr.size(); ++i) {
+  for (auto i = 1; i < arr.size(); ++i) {
     ss << " + " << arr[i].real() << "x^" << i;
   }
   return ss.str();
